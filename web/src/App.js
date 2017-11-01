2b622d9bdd691ae3e442b8764819386e46037f72
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-11-01 12:30:58 +0100

    Add React app

---
+++ web/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import store from 'store'
+
+import Home from 'components/pages/Home'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    )
+  }
+}

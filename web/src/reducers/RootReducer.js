d8b02fe3d057afda5acc689ae3264cd880d19e09
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-11-01 13:15:19 +0100

    Fix webpack pipeline

--- web/src/reducers/RootReducer.js
+++ web/src/reducers/RootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import { reducer as ApiReducer } from 'redux-bees'
 
 import AppReducer from 'reducers/AppReducer'
 
 const RootReducer = combineReducers({
   app: AppReducer,
-  api: ApiReducer,
 })
 
 export default RootReducer

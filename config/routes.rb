564e9e0a5ef1abcbd4793b13bfcdab90df720aa4
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-11-07 15:00:40 +0100

    Add User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ##
+  # API endpoints
+  #
+  scope :api, :constraints => { :format => :json } do
+    # /api/users
+    resources :users, :only => %i[create show update]
+  end
 end

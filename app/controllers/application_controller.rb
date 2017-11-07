08c6bc6d95881bb7a2b67cf3247e2304b0bd0877
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-11-07 15:18:33 +0100

    Enforce JSON requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :ensure_json_request
+
+  private
+
+  def ensure_json_request
+    return if request.format == :json
+    head :not_acceptable
+  end
 end

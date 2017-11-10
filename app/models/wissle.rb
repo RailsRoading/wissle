6e5b918397cbaf2c981b33ba42191344692a6171
Michael T <asy-dk@live.dk>
2017-11-10 22:08:35 +0100

    added Wissle validations
    Added validations to longitude, and latitude.

--- app/models/wissle.rb
+++ app/models/wissle.rb
@@ -1,3 +1,29 @@
+# frozen_string_literal: true
+
 class Wissle < ApplicationRecord
+  ##
+  # Associations
+  #
   belongs_to :user
+
+  ##
+  # Validations
+  #
+  validates :longitude,
+            :presence => true
+
+  validates :latitude,
+            :presence => true
+  ##
+  # Callbacks
+  #
+  ##
+  # Methods
+  #
+  ##
+  # Overrides
+  #
+  ##
+  # Helpers and callback methods
+  #
 end

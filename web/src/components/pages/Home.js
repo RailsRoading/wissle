7b3b695de450b646f14aa44d40f74d9d9d1e19fb
Christian Billeschou Kjær <fc-christian@hotmail.com>
2017-11-23 08:32:19 +0100

    MyMapComponent

--- web/src/components/pages/Home.js
+++ web/src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button } from 'react-foundation'
+import MyMapComponent from 'components/MyMapComponent'
 
 import { Link } from 'react-router-dom'
 
@@ -8,7 +9,7 @@ export default class Home extends Component {
     return (
       <div>
         <h1>Home</h1>
-
+        <MyMapComponent/>
         <Link to="/details"><Button>Details</Button></Link>
       </div>
     );

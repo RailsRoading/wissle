2395dd187ef072a532b3e4d611fc37a3e94d5be3
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-11-23 14:44:48 +0100

    Fixed MyMapComponent
    "Home" now displays the map from the MyMapComponent, and the default map coordinates and default zoom have been hardcoded.

--- web/src/components/MyMapComponent.js
+++ web/src/components/MyMapComponent.js
@@ -1,19 +1,10 @@
 import React from "react"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) =>
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={15}
+    defaultCenter={{ lat: 57.049515, lng: 9.916286 }}
   >
     {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
   </GoogleMap>
@@ -48,3 +39,5 @@ class MyFancyComponent extends React.PureComponent {
     )
   }
 }
+
+export default MyMapComponent
--- web/src/components/pages/Home.js
+++ web/src/components/pages/Home.js
@@ -8,9 +8,15 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
-        <MyMapComponent/>
-        <Link to="/details"><Button>Details</Button></Link>
+        <h1>Wissle</h1>
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      {/*<Link to="/details"><Button>Details</Button></Link>*/}
       </div>
     );
   }

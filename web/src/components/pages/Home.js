73383331eb9195ec60ff3862976967d3a948bd35
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-11-29 11:58:17 +0100

    styled WissleLogo & pageDiv sizes

--- web/src/components/pages/Home.js
+++ web/src/components/pages/Home.js
@@ -9,12 +9,12 @@ export default class Home extends Component {
   render() {
     return (
       <div className="pageDiv">
-        <img src="WissleLogo.JPG" alt="WissleLogo"/>
+        <img src="WissleLogo.JPG" alt="Wissle logo" className="WissleLogo"/>
         <MyMapComponent
           isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
       {/*<Link to="/details"><Button>Details</Button></Link>*/}

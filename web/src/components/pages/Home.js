235bf6f0058b94ec2e3376689c8e09c5177a5a14
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-11-29 11:19:50 +0100

    added index.css

--- web/src/components/pages/Home.js
+++ web/src/components/pages/Home.js
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom'
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <img src="WissleLogo.JPG" alt="Wissle logo"/>
+      <div className="pageDiv">
+        <img src="WissleLogo.JPG" alt="WissleLogo"/>
         <MyMapComponent
           isMarkerShown
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"

50e4a97cbdaab6ae1854a9f818cdbfb120991b48
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-04 13:51:43 +0100

    Add category seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.transaction do
+  %w[
+    Coffee
+    Drinks
+    Food
+    Sports
+    Games
+    Film
+    Art
+    Literature
+    Music
+    Party
+    Other
+  ].each do |title|
+    Category.create! :title => title
+  end
+end

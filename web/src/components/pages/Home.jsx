dfcd2a8b9d4b8a5ab929759c062d5980ed6cf948
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-12-06 13:29:01 +0100

    Added grid, changed map size and map page layout

--- web/src/components/pages/Home.jsx
+++ web/src/components/pages/Home.jsx
@@ -4,27 +4,24 @@ import { Button } from 'react-foundation'
 import Protected from 'components/Protected'
 import MyMapComponent from 'components/MyMapComponent'
 
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default class Home extends Component {
   render() {
-    return (
-      <Protected>
-        <div className="pageDiv">
-          <img src="WissleLogo.JPG" alt="Wissle logo" className="WissleLogo"/>
-          <MyMapComponent
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `500px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
-          <Link to="/details"><Button>Details</Button></Link>
-          <Link to="/user"><Button>User</Button></Link>
-          <Link to="/wissle"><Button>Wissle</Button></Link>
-          <Link to="/tag"><Button>Tag</Button></Link>
+    return (<div className="grid-x grid-home align-middle">
+      <div className="small-12 medium-12 large-12 cell text-center">
+        <div>
+          <MyMapComponent isMarkerShown="isMarkerShown" googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `100vh` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>
         </div>
-      </Protected>
-    )
+        { /* Add these to the Hamburger Menu!
+        <Link to="/details">
+          <Button>Details</Button>
+        </Link>
+        <Link to="/user">
+          <Button>User</Button>
+        </Link>
+        */ }
+      </div>
+    </div>);
   }
 }

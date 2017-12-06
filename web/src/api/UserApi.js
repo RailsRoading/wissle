963a55efac6df6655856da7512d7a331cd17e76e
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-06 10:25:56 +0100

    Reformat UserApi

--- web/src/api/UserApi.js
+++ web/src/api/UserApi.js
@@ -1,37 +1,31 @@
-import delay from './delay';
+import delay from './delay'
 import config from 'config'
 
 class UserApi {
+  static saveUser(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        fetch(`${config.API_ENDPOINT}/api/users`, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            data: {
+              username: user.username,
+              age: user.age
+            }
+          })
+        })
+          .then((response) => {
+            //do something awesome that makes the world a better place
+          })
 
-    static saveUser(user) {
-
-        return new Promise((resolve, reject) => {
-
-            setTimeout(() => {
-
-                fetch(`${config.API_ENDPOINT}/api/users`, {
-                    method: "post",
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
-                    },
-
-                    body: JSON.stringify({
-                        data: {
-                            username: user.username,
-                            age: user.age
-                        }
-                    })
-                })
-                    .then( (response) => {
-                        //do something awesome that makes the world a better place
-                    });
-
-                resolve(Object.assign({}, user));
-            }, delay);
-
-        });
-    }
+        resolve(Object.assign({}, user))
+      }, delay)
+    })
+  }
 }
 
-export default UserApi;
+export default UserApi

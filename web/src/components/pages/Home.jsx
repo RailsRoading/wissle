7053f214fdd4ea6fc47dffccf61de709dd9340f9
Patrick Ellermann Gorgin <p.gorgin@gmail.com>
2017-12-11 11:47:41 +0100

    Functional overlay hamburger menu
    work in progress

--- web/src/components/pages/Home.jsx
+++ web/src/components/pages/Home.jsx
@@ -1,19 +1,46 @@
 import React, {Component} from 'react'
 import {Icon, Menu, MenuItem} from 'react-foundation'
 import MyMapComponent from 'components/MyMapComponent'
-{/* import {Link} from 'react-router-dom' */}
 
 export default class Home extends Component {
+  constructor(props, context) {
+    super(props, context)
+    this.openMenu = this.openMenu.bind(this)
+    this.state = {
+      openMenu: false
+    }
+  }
+  openMenu() {
+    this.setState({
+      openMenu: !this.state.openMenu
+    })
+  }
   render() {
     return (<div className="grid-x grid-home align-middle">
       <div className="small-12 medium-12 large-12 cell text-center">
-        <div className="hamburgerBars">
-          <Menu>
-            <MenuItem>
-              <i class="fa fa-bars" aria-hidden="true"></i>
-            </MenuItem>
-          </Menu>
+        <div className="hamburger">
+          <nav role="navigation">
+            <ul id="hamburgerMenu" className={`hamburgerMenu ${this.state.openMenu ? 'is-open': ""}`}>
+              <li>
+                <a href="interests">Tags</a>
+              </li>
+              <li>
+                <a href="wissles">Wissles</a>
+              </li>
+              <li>
+                <a href="user">User</a>
+              </li>
+              <li>
+                <a href="help">Help</a>
+              </li>
+            </ul>
+            <a data-toggle-menu="data-toggle-menu" className="hamburgerMenu-toggle" href="#hamburgerMenu" onClick={this.openMenu}>
+              <i className="fa fa-bars"></i>
+              {/* <i className="fa fa-plus"></i> */}
+            </a>
+          </nav>
         </div>
+
         <div>
           <MyMapComponent isMarkerShown="isMarkerShown" googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places" loadingElement={<div style = {{ height: `100%` }}/>} containerElement={<div style = {{ height: `100vh` }}/>} mapElement={<div style = {{ height: `100%` }}/>}/>
         </div>

32d865594162ccabb44b4d64f84537471ef4b25a
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-11 11:27:56 +0100

    Add GET API request

--- web/src/api/Api.js
+++ web/src/api/Api.js
@@ -1,5 +1,18 @@
 import config from 'config'
 
+export function get(endpoint, uuid) {
+  return fetch(`${config.API_ENDPOINT}${endpoint}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': uuid,
+    },
+  }).then((response) => {
+    return response.json()
+  })
+}
+
 export function post(endpoint, body) {
   return fetch(`${config.API_ENDPOINT}${endpoint}`, {
     method: 'POST',
@@ -7,7 +20,7 @@ export function post(endpoint, body) {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
   }).then((response) => {
     return response.json()
   })

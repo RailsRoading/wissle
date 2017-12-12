547fd4b0b578b54eea446d76061e04d7850d1ec8
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-12 09:02:44 +0100

    Fix unmerged file

--- web/src/store.js
+++ web/src/store.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { compose, createStore, applyMiddleware } from 'redux'
 import persistState from 'redux-localstorage'
-=======
-import { createStore, applyMiddleware, compose } from 'redux'
->>>>>>> Add location in state tree
 import thunk from 'redux-thunk'
 
 import RootReducer from 'reducers/RootReducer'

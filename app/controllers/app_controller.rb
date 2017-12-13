ae9624faacbdc0f62f9934e2441bf4215bb59b27
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-13 12:01:54 +0100

    Add app index route

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class AppController < ActionController::Base
+  # GET /
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root :to => 'app#index'
+
   ##
   # API endpoints
   #

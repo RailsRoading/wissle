28548abd97d3ff3c53f3b6920d8fca79b6a043c8
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-13 16:14:13 +0100

    Add show wissle modal

---
+++ app/assets/javascripts/wissles.js
@@ -0,0 +1,22 @@
+function getWissle(id) {
+  var $modal = new Foundation.Reveal($('#modal-show-wissle'));
+  $modal.open();
+
+  $.ajax({
+    url: '/api/wissles',
+    dataType: 'json',
+    contentType: 'json',
+    headers: {
+      'Authorization': window.localStorage.getItem('user.uuid')
+    },
+    success: function(data) {
+      var wissle = data[0].data;
+
+      $('#modal-show-wissle-text').html(wissle.text);
+      $('#modal-show-wissle-latitude').html(wissle.latitude);
+      $('#modal-show-wissle-longitude').html(wissle.longitude);
+    },
+    error: function(error) {
+    }
+  })
+}

a9c457d7272adf964fa60d9c36508dff5768c8bb
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-14 15:08:39 +0100

    Extract API functions to api folder

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,30 +2,10 @@ $(document).ready(function() {
   $('#form-user').submit(function(e) {
     e.preventDefault();
 
-    $.ajax({
-      url: '/api/users',
-      dataType: 'json',
-      contentType: 'application/json',
-      method: 'POST',
-      headers: {
-        'Authorization': window.localStorage.getItem('uuid')
-      },
-      data: JSON.stringify({
-        data: {
-          username: $(this).children('[name=username]').val(),
-          age: $(this).children('[name=age]').val()
-        }
-      }),
-      success: function(data) {
-        window.localStorage.setItem('user.id', data.data.id);
-        window.localStorage.setItem('user.uuid', data.data.uuid);
-
-        window.location.replace('/tags');
-      },
-      error: function(error) {
-        showAlert(error.responseJSON.errors.join(', '));
-      }
-    })
-
+    createUser($(this).children('[name=username]').val(),
+               $(this).children('[name=age]').val(),
+               function() {
+                 window.location.replace('/tags');
+               })
   });
 });

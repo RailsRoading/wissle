25ccb94dee350962d71f564e74ed4866d80b8499
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-14 09:42:40 +0100

    Submit user form

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,22 +1,29 @@
-function createUser() {
-  $.ajax({
-    url: '/api/users',
-    dataType: 'json',
-    contentType: 'application/json',
-    method: 'POST',
-    data: JSON.stringify({
-      data: {
-        username: 'Florian',
-        age: '23'
+$(document).ready(function() {
+  $('#form-user').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: '/api/users',
+      dataType: 'json',
+      contentType: 'application/json',
+      method: 'POST',
+      headers: {
+        'Authorization': window.localStorage.getItem('uuid')
+      },
+      data: JSON.stringify({
+        data: {
+          username: $(this).children('[name=username]').val(),
+          age: $(this).children('[name=age]').val()
+        }
+      }),
+      success: function(data) {
+        window.localStorage.setItem('user.id', data.data.id);
+        window.localStorage.setItem('user.uuid', data.data.uuid);
+      },
+      error: function(error) {
+        showAlert(error);
       }
-    }),
-    success: function(data) {
-      window.localStorage.setItem('user.id', data.data.id);
-      window.localStorage.setItem('user.uuid', data.data.uuid);
-      window.location.replace('/tags');
-    },
-    error: function(error) {
-      showAlert(error);
-    }
-  })
-}
+    })
+
+  });
+});

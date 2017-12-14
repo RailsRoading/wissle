22031c29dff8bab97975650150290d6e6fa56bd4
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-14 09:48:43 +0100

    Redirect when user created

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -19,6 +19,8 @@ $(document).ready(function() {
       success: function(data) {
         window.localStorage.setItem('user.id', data.data.id);
         window.localStorage.setItem('user.uuid', data.data.uuid);
+
+        window.location.replace('/tags');
       },
       error: function(error) {
         showAlert(error.responseJSON.errors.join(', '));

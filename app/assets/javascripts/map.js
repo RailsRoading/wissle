a0542a1ad7e5cddac36357c3c1b7c8c156c8736e
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-12-14 10:58:30 +0100

    Add geolocation feature

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,14 +1,44 @@
-function initMap() {
-  var aalborg = {
+var position = {
+  // Default position
+  default: {
     lat: 57.049515,
     lng: 9.916286
-  };
+  },
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  // Current location
+  current: null,
+
+  // Map
+  map: null
+};
+
+function initMap() {
+  position.map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: aalborg,
+    center: position.default,
     disableDefaultUI: true
   });
 
-  var marker = new google.maps.Marker({position: aalborg, map: map});
+  // Geolocate every 5 seconds
+  setTimeout(geolocate, 5000);
+
+  geolocate();
+}
+
+function geolocate() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      var newPosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+      if (position.current) {
+        position.current.setPosition(newPosition);
+      } else {
+        position.current = new google.maps.Marker({ position: newPosition, map: position.map, icon: '/assets/bluedot.png' });
+      }
+
+      position.map.panTo(newPosition);
+    });
+  } else {
+    showAlert('Location is not supported')
+  }
 }
